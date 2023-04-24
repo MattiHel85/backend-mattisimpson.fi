3074ad49d3d3755b2a83220f26fbc01005371d8e
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-04-24 16:58:11 +0300

    Add todos as comments

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+// Add mongodb/mongoose
+
+// Create user, blog, and project schemas
+
+// Create routes
+
 // Express server
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
